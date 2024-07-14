e22e3215658f7f363cb247127315d7d070c67f29
FiaT <fnight644@gmail.com>
2024-07-15 00:55:44 +0700

    tao trang hang hoa va cac model lien quan

---
+++ EcommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,38 @@
+﻿using EcommerceMVC.Data;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly HshopContext db;
+
+        public HangHoaController(HshopContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas = db.HangHoas.AsSingleQuery();
+
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hangHoas.Select(p => new HangHoaViewModel
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,    
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            
+            return View(result);
+        }
+    }
+}
---
+++ EcommerceMVC/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceMVC.Data;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        public readonly HshopContext db;
+        public MenuLoaiViewComponent(HshopContext context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(lo => new MenuLoaiViewModel
+                {
+                    MaLoai = lo.MaLoai,
+                    TenLoai = lo.TenLoai,
+                    SoLuong = lo.HangHoas.Count
+                }
+            ).OrderBy(p => p.TenLoai);
+            return View( data );
+        }
+    }
+}
